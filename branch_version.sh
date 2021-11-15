17673abdc6a8c17541a1c8ee6ec741744b0e83bf
Aleh Mudrak <aleh.mudrak@itechart-group.com>
2021-11-15 18:37:21 +0300

    Test.

--- branch_version.sh
+++ branch_version.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 set -euo pipefail
 
+DOCKER_IMAGE="alehmudrak/github-action:"
+
 [ -d /version/ ] || sudo mkdir -p /version/
 
-# git describe --tags --abbrev=0 | sed 's/[^.0-9]*//g' > version.txt
 COMMIT=$(git log -1 --pretty=tformat:%h)
 BRANCH=$(git branch --show-current)
-TEGVERSION=$(git describe --tags --abbrev=0 | sed 's/[^.0-9]*//g')
 echo $DOCKER_IMAGE$BRANCH-$COMMIT > version.txt
-# echo $DOCKER_IMAGE$TEGVERSION > version.txt
 
 sudo mv version.txt /version/
