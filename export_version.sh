552a2e902ab01b46d64041aacb9b1a3d18d59cc9
Aleh Mudrak <aleh.mudrak@itechart-group.com>
2021-11-15 16:12:01 +0300

    Change path.

--- export_version.sh
+++ export_version.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
+
 # --- For test --- delete ---
 # cd ../github-action/
 # DOCKER_IMAGE="alehmudrak/github-action:"
 # --- delete ---
 
-cd ..
+sudo mkdir /version/
+cd $HOME/github-action/
 COMMIT=$(git log -1 --pretty=tformat:%h)
 BRANCH=$(git branch --show-current)
 LASTTAG=$(git describe --tags --abbrev=0 | sed 's/[^.0-9]*//g')
 echo $DOCKER_IMAGE$LASTTAG > version.txt
-echo "Project version: $DOCKER_IMAGE$LASTTAG"
-echo "::set-output name=PROJECT_VERSION::$DOCKER_IMAGE$LASTTAG"
+sudo mv version.txt /version/
+
+# echo "Project version: $DOCKER_IMAGE$LASTTAG"
+# echo "::set-output name=PROJECT_VERSION::$DOCKER_IMAGE$LASTTAG"
