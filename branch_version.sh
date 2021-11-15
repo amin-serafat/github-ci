2f86eb5775f2ea590646aa5886b8617543bf5cf6
Aleh Mudrak <aleh.mudrak@itechart-group.com>
2021-11-15 18:18:19 +0300

    Added tag and branch scripts.

---
+++ branch_version.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -euo pipefail
+
+[ -d /version/ ] || sudo mkdir -p /version/
+
+# git describe --tags --abbrev=0 | sed 's/[^.0-9]*//g' > version.txt
+COMMIT=$(git log -1 --pretty=tformat:%h)
+BRANCH=$(git branch --show-current)
+TEGVERSION=$(git describe --tags --abbrev=0 | sed 's/[^.0-9]*//g')
+echo $DOCKER_IMAGE$BRANCH-$COMMIT > version.txt
+# echo $DOCKER_IMAGE$TEGVERSION > version.txt
+
+sudo mv version.txt /version/
--- version.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -euo pipefail
-
-[ -d /version/ ] || sudo mkdir -p /version/
-git describe --tags --abbrev=0 | sed 's/[^.0-9]*//g' > version.txt
-sudo mv version.txt /version/
