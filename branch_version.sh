4e28b4e7867476512dd61cbf20d118e181fe85e2
Aleh Mudrak <aleh.mudrak@itechart-group.com>
2021-11-15 18:46:33 +0300

    Change scripts.

--- branch_version.sh
+++ branch_version.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 set -euo pipefail
 
-DOCKER_IMAGE="alehmudrak/github-action:"
-
 [ -d /version/ ] || sudo mkdir -p /version/
 
+DOCKER_HUB=$(sudo cat /version/DOCKER_HUB)
 COMMIT=$(git log -1 --pretty=tformat:%h)
 BRANCH=$(git branch --show-current)
-echo $DOCKER_IMAGE$BRANCH-$COMMIT > version.txt
+echo $DOCKER_HUB$BRANCH-$COMMIT > version.txt
 
 sudo mv version.txt /version/
--- teg_version.sh
+++ teg_version.sh
@@ -3,11 +3,9 @@ set -euo pipefail
 
 [ -d /version/ ] || sudo mkdir -p /version/
 
-# git describe --tags --abbrev=0 | sed 's/[^.0-9]*//g' > version.txt
-COMMIT=$(git log -1 --pretty=tformat:%h)
-BRANCH=$(git branch --show-current)
+DOCKER_HUB=$(sudo cat /version/DOCKER_HUB)
 TEGVERSION=$(git describe --tags --abbrev=0 | sed 's/[^.0-9]*//g')
-# echo $DOCKER_IMAGE$BRANCH-$COMMIT > version.txt
-echo $DOCKER_IMAGE$TEGVERSION > version.txt
+# git describe --tags --abbrev=0 | sed 's/[^.0-9]*//g' > version.txt
+echo $DOCKER_HUB$TEGVERSION > version.txt
 
 sudo mv version.txt /version/
