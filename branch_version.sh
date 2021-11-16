616af0cd78391e55dcd9c291a56687fe6aef15d1
Aleh Mudrak <aleh.mudrak@itechart-group.com>
2021-11-16 13:30:02 +0300

    Correct branch-version.

--- branch_version.sh
+++ branch_version.sh
@@ -3,8 +3,8 @@ set -euo pipefail
 
 [ -d /version/ ] || sudo mkdir -p /version/
 
-DOCKERHUBACCOUNT=$(echo DOCKERHUBACCOUNT.txt)
-DOCKERREPOSITORY=$(echo DOCKERREPOSITORY.txt)
+BRANCH=$(echo DOCKERHUBACCOUNT.txt)
+COMMIT=$(echo DOCKERREPOSITORY.txt)
 
 TEGVERSION=$(echo "$BRANCH-$COMMIT")
 echo $TEGVERSION > teg_version.txt
