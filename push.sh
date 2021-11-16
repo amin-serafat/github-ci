c2761083430419e336515cc2be0608182e065fb4
Aleh Mudrak <aleh.mudrak@itechart-group.com>
2021-11-16 10:36:57 +0300

    Change code in push.sh.

--- push.sh
+++ push.sh
@@ -3,7 +3,7 @@ set -euo pipefail
 
 echo "--- Push image ---"
 IMAGE_NAME=$(cat /version/version.txt)
-DOCKERHUB_USERNAME=$(cat /version/DOCKERHUB_USERNAME.txt)
-DOCKERHUB_TOKEN=$(cat /version/DOCKERHUB_USERNAME.txt)
+# DOCKERHUB_USERNAME=$(cat /version/DOCKERHUB_USERNAME.txt)
+# DOCKERHUB_TOKEN=$(cat /version/DOCKERHUB_USERNAME.txt)
 
 docker push $IMAGE_NAME
