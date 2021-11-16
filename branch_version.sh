5e258c1512e5b15fd91e2a9e09cfe02628c7d688
Aleh Mudrak <aleh.mudrak@itechart-group.com>
2021-11-16 15:01:37 +0300

    Change some errors in scripts.

--- branch_version.sh
+++ branch_version.sh
@@ -3,7 +3,8 @@ set -euo pipefail
 
 echo "--- Create Tag by Branch and Last Commit image ---"
 
-BRANCH=$(cat DOCKERHUBACCOUNT.txt)
-COMMIT=$(cat DOCKERREPOSITORY.txt)
+BRANCH=$(git branch --show-current)
+COMMIT=$(git log -1 --pretty=tformat:%h)
 
 echo "$BRANCH-$COMMIT" > teg_version.txt
+sudo mv *.txt /version/
--- teg_version.sh
+++ teg_version.sh
@@ -6,3 +6,4 @@ echo "--- Create Tag by Branch Tag ---"
 TEGVERSION=$(git describe --tags --abbrev=0 | sed 's/[^.0-9]*//g')
 
 echo $TEGVERSION > teg_version.txt
+sudo mv *.txt /version/
