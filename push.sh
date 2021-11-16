5e98ac1ef628135f674856c1ff1c3069b391d4fb
Aleh Mudrak <aleh.mudrak@itechart-group.com>
2021-11-16 10:36:39 +0300

    Change code in push.sh.

--- push.sh
+++ push.sh
@@ -6,5 +6,4 @@ IMAGE_NAME=$(cat /version/version.txt)
 DOCKERHUB_USERNAME=$(cat /version/DOCKERHUB_USERNAME.txt)
 DOCKERHUB_TOKEN=$(cat /version/DOCKERHUB_USERNAME.txt)
 
-docker login -u "${{ secrets.DOCKERHUB_USERNAME }}" -p "${{ secrets.DOCKERHUB_TOKEN }}"
 docker push $IMAGE_NAME
